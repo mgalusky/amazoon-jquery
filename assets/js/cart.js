ffc30e8b839f280325b081cfb7b2407826092955
Melissa Galusky <melissa.galusky@gmail.com>
2016-02-11 20:10:40 -0600

    step 2 of checkout page

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -27,20 +27,69 @@ $(document).ready(function(){
 
     for(var i = 0; i < products.length; i++){
 
+      var updateView = function(){
+        // 1: Update the cart's total
+        // 2: Hide/show the .empty h4 tags based on the total products in the car or the saved for later section
+        var productsInCart = $('.cart .product');
+        var productsSaved = $('.saved .product');
+
+        if(productsInCart.length === 0) {
+          // There are no products in the cart
+          // Hide cart total
+          $('.cart-total').hide();
+          // Show H3 tag for cart empty
+          $('.cart .empty').show();
+          // Hide Item/Price header
+          $('.headers').hide();
+        } else {
+          // There ARE products in the cart
+          // Show cart total
+          $('.cart-total').show();
+          // Hide H3 tag for cart empty
+          $('.cart .empty').hide();
+        }
+
+        if(productsSaved.length === 0){
+          //There are no products in the saved for later section
+          //Show H3 tag for saved for later empty
+          $('.saved .empty').show();
+          // $('.headers').hide();
+
+        } else {
+          // There ARE products in teh saved for later section
+          // Hide H3 tag for saved for later empty
+          $('.saved .empty').hide();
+        }
+        var total = 0;
+        $(productsInCart).each(function(){
+          total += Number($(this).data('price'));
+        });
+        // console.log(total); update <span/> in cart total
+        $('.cart-total span').text('$' + total);
+      }
+
       var newItem = $(productTemplate);
 
-      $('img', newItem).attr('src', $(.text(products[i].imageSrc);
-      $('.blue', newItem).text(products[i].name);
-      $('.manufacturer', newItem).text(products[i].manufacturer);
-      $('h3', newItem).text(products[i].description)
-      $('.mobile-price', newItem).text(products[i].price);
+      $(newItem).data('price', products[i].price);
+      $('img', newItem).attr('src', products[i].imageSrc);
+      $('.content h2', newItem).text(products[i].name);
+      $('.content h2', newItem).append($('<span/>').addClass('manufacturer').text('by ' + products[i].manufacturer));
+      $('h3', newItem).text(products[i].description);
+      $('.mobile-price, .price', newItem).text('$' + products[i].price);
 
-      $('.products').append(newItem);
+      $('.cart').append(newItem);
     }
+      updateView();
 
-      $('move').on('.submit', function(event) {
-        $('.saved').append(this);
-        if ('.cart-total h1.orange' === 'Save for later') {
-          $('.cart-total h1.orange').text('Add to cart')
-      } else $('.cart-total h1.orange').text('Save for later')
+      $('.move').on('click', function(){
+        if ($(this).parents().eq(4).hasClass('cart')) {
+          $('.saved').append($(this).parents().eq(3));
+          $($(this)).text('Add to cart')
+          updateView();
+        } else {
+          $('.cart').append($(this).parents().eq(3));
+          $($(this)).text('Save for later');
+          updateView();
+        }
+      });
   });
--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  // Get started here!
+  $('.order-form').on('submit', function(event){
+    event.preventDefault();
+    // if input has attribute required
+    var requiredFields = $('input[required]');
+    // use each();
+    $(requiredFields).each(function(){
+      // check if length of text is 0
+      if ($(this).last().val().length === 0) {
+        // add .has-error class to this input's parent .form-group
+        $(this).parent.addClass('has-error');
+    }
+  })
 });
